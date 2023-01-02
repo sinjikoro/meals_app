c8051fe927376e36309d91fc80dac6c4577a8f9d
shinnaga-icd <shinji.nagasaki@infcurion.com>
2023-01-02 17:31:42 +0900

    161. Registering a Screen as the Main Screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -7,17 +7,22 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
       ),
-      children: DAMMY_CATRGORIES
-          .map((catData) =>
-              CategoryItem(title: catData.title, color: catData.color))
-          .toList(),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DAMMY_CATRGORIES
+            .map((catData) =>
+                CategoryItem(title: catData.title, color: catData.color))
+            .toList(),
+      ),
     );
   }
 }
--- lib/dammy_data.dart
+++ lib/dammy_data.dart
@@ -7,4 +7,8 @@ const DAMMY_CATRGORIES = [
   Category(id: 'c2', title: 'French', color: Colors.lightBlue),
   Category(id: 'c3', title: 'Chinese', color: Colors.limeAccent),
   Category(id: 'c4', title: 'Japanese', color: Colors.red),
+  Category(id: 'c5', title: 'Restrant', color: Colors.pink),
+  Category(id: 'c6', title: 'Burger', color: Colors.yellowAccent),
+  Category(id: 'c7', title: 'Noodle', color: Colors.teal),
+  Category(id: 'c8', title: 'Bread', color: Colors.amber),
 ];
