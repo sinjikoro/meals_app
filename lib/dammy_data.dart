ea885ea3c062c9a2886e2006cc357a08d3e8c43c
shinnaga-icd <shinji.nagasaki@infcurion.com>
2023-01-02 17:06:46 +0900

    159. Starting With The App - Adding Base Data

---
+++ lib/dammy_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DAMMY_CATRGORIES = [
+  Category(id: 'c1', title: 'Itarian', color: Colors.purple),
+  Category(id: 'c2', title: 'French', color: Colors.lightBlue),
+  Category(id: 'c3', title: 'Chinese', color: Colors.limeAccent),
+  Category(id: 'c4', title: 'Japanese', color: Colors.red),
+];
