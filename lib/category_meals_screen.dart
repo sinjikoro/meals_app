191c3bd1456b495dbaca0e0643253458b98cf368
sinjikoro <shinnaga.dev@gmail.com>
2023-01-07 01:02:24 +0900

    163. Navigating to a New Page

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  const CategoryMealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('The Recipes'),),
+      body: const Center(child: Text('The Recipes for the category.')),
+    );
+  }
+}
