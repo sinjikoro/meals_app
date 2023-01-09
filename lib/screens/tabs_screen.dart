9111b79bf0766b658dcecfcb855271cd94456625
sinjikoro <shinnaga.dev@gmail.com>
2023-01-10 01:06:55 +0900

    175. Adding a Bottom TabBar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -11,30 +11,45 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = const <Map<String, Object>>[
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavoriteScreen(), 'title': 'Your Favorite'},
+  ];
+  int _selectPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Meals'),
-            bottom: const TabBar(
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.category),
-                  text: 'Categories',
-                ),
-                Tab(
-                  icon: Icon(Icons.star),
-                  text: 'Favorite',
-                ),
-              ],
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectPageIndex]['title'] as String),
+      ),
+      body: _pages[_selectPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.greenAccent,
+        selectedItemColor: Theme.of(context).selectedRowColor,
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _selectPageIndex,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: const Icon(Icons.category),
+            label: 'Categories',
           ),
-          body: const TabBarView(children: [
-            CategoriesScreen(),
-            FavoriteScreen(),
-          ]),
-        ));
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: const Icon(Icons.star),
+            label: 'Favorite',
+          ),
+        ],
+      ),
+    );
   }
 }
